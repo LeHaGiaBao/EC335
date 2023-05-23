e5d357cb21e77d7e8f2cc0ff1700ac31520aadbf
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-05-24 00:18:59 +0700

    Update frontend project

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 const corsOptions = {
-	origin: 'http://localhost:8081',
+	origin: 'http://localhost:3000',
 }
 
 app.use(express.json())
